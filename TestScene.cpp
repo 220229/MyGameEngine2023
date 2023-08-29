42af2fd2e226dc9c76b2f307ad6615dea89914f0
壮太 佐藤 <220229@jc-21.jp>
2023-08-29 11:29:40 +0900

    15x15 block

--- TestScene.cpp
+++ TestScene.cpp
@@ -1,6 +1,9 @@
 #include "TestScene.h"
 #include "Engine/Input.h"
 #include "Engine/SceneManager.h"
+#include "Engine/Camera.h"
+#include "Engine/Input.h"
+#include "Stage.h"
 
 TestScene::TestScene(GameObject* parent)
 	:GameObject(parent, "TestScene")
@@ -9,6 +12,10 @@ TestScene::TestScene(GameObject* parent)
 
 void TestScene::Initialize()
 {
+	Camera::SetPosition(XMFLOAT3(0, 4, -8));
+	Camera::SetTarget(XMFLOAT3(0, 2, 0));
+
+	Instantiate<Stage>(this);
 }
 
 void TestScene::Update()
